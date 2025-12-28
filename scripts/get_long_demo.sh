1593bce499cdde7cdfdac4df24269302445c546c
NizamMohammed9 <nmoham18@depaul.edu>
2025-12-28 14:35:47 -0600

    update tool name in scripts/get_long_demo.sh

--- scripts/get_long_demo.sh
+++ scripts/get_long_demo.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 mkdir -p samples
 
-URL="https://github.com/Nizamohammed/Voice-Transcribe/releases/download/v0.1.0/oct_23_psychology_02.mp3"
+URL="https://github.com/Nizamohammed/Scribepad/releases/download/v0.1.0/oct_23_psychology_02.mp3"
 OUT="samples/demo_long_34min.mp3"
 
 echo "Downloding long demo audio...."
